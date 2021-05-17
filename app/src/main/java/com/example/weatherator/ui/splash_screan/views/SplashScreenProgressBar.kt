a4b632e38e00db0159f117bc2eedfaa264844b50
Rafal Halasa <rafal.halasa@gmail.com>
2021-05-17 15:17:30 +0200

    add build versions

---
+++ app/src/main/java/com/example/weatherator/ui/splash_screan/views/SplashScreenProgressBar.kt
@@ -0,0 +1,9 @@
+package com.example.weatherator.ui.splash_screan.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ProgressBar
+
+class SplashScreenProgressBar(context: Context?, attrs: AttributeSet?) :
+    ProgressBar(context, attrs) {
+}
